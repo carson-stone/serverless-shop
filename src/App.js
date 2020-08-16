f09c50e5ec7d52273ad5e39a3b332fa06022fcd9
cstone <cstone@cs.odu.edu>
2020-08-16 16:16:55 -0400

    store and use session in aws amplify

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Auth } from 'aws-amplify';
 import './App.css';
 import { AppContext } from './libs/contextLib';
 import Nav from './components/Nav';
@@ -6,19 +7,39 @@ import Sidebar from './components/Sidebar';
 import Routes from './Routes';
 
 function App() {
+  const [isAuthenticating, setAuthenticating] = useState(true);
   const [isAuthenticated, setAuthenticated] = useState(false);
   const [sidebarOpen, setSidebar] = useState(false);
   const openSidebar = () => setSidebar(true);
   const closeSidebar = () => setSidebar(false);
 
+  async function loadSession() {
+    try {
+      await Auth.currentSession();
+      setAuthenticated(true);
+    } catch (error) {
+      if (error !== 'No current user') {
+        alert(error);
+      }
+    } finally {
+      setAuthenticating(false);
+    }
+  }
+
+  useEffect(() => {
+    loadSession();
+  }, []);
+
   return (
-    <div className='App'>
-      <Nav openSidebar={openSidebar} />
-      <AppContext.Provider value={{ isAuthenticated, setAuthenticated }}>
-        <Sidebar open={sidebarOpen} closeSidebar={closeSidebar} />
-        <Routes />
-      </AppContext.Provider>
-    </div>
+    !isAuthenticating && (
+      <div className='App'>
+        <Nav openSidebar={openSidebar} />
+        <AppContext.Provider value={{ isAuthenticated, setAuthenticated }}>
+          <Sidebar open={sidebarOpen} closeSidebar={closeSidebar} />
+          <Routes />
+        </AppContext.Provider>
+      </div>
+    )
   );
 }
 
