71840c2c618f20d06a7cf1b0e6c0563c526a2885
cstone <cstone@cs.odu.edu>
2020-08-27 18:47:38 -0400

    fixed product card click bug

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -6,12 +6,17 @@ export default function ({ src, name, price }) {
   const history = useHistory();
 
   return (
-    <div
-      className='ProductCard'
-      onClick={() => history.push(`/products/${name}`)}
-    >
-      <img src={src} width='350' alt={name} />
-      <div className='product-info'>
+    <div className='ProductCard'>
+      <img
+        src={src}
+        width='350'
+        alt={name}
+        onClick={() => history.push(`/products/${name}`)}
+      />
+      <div
+        className='product-info'
+        onClick={() => history.push(`/products/${name}`)}
+      >
         <div>{name}</div>
         <div>
           <span>$ {price}</span>
