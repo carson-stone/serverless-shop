f44e490d6f1aac39360ffc776ed8c1d2e50d1483
cstone <cstone@cs.odu.edu>
2020-08-27 19:03:31 -0400

    pull product image and price from app context

--- src/containers/ProductPage.js
+++ src/containers/ProductPage.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useParams, useHistory, Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useParams, Link } from 'react-router-dom';
 import './ProductPage.css';
 import { useAppContext } from '../libs/contextLib';
 import backIcon from '../assets/back-icon.png';
@@ -7,9 +7,17 @@ import Chip from '../components/Chip';
 
 export default function ProductPage() {
   const { name } = useParams();
-  const history = useHistory();
+  const { products } = useAppContext();
+  const [product, setProduct] = useState(null);
 
-  return (
+  useEffect(() => {
+    const p = products.find((p) => name === p.name);
+    setProduct(p);
+  }, [products]);
+
+  return !product ? (
+    <></>
+  ) : (
     <div className='ProductPage container'>
       <span>
         <Link to='/'>
@@ -18,8 +26,18 @@ export default function ProductPage() {
         <h1>{name}</h1>
       </span>
       <span>
+        <div className='price-and-rating'>
+          <p>{product.price}</p>
+          <p>*****</p>
+        </div>
         <Chip text='add to cart' primary />
         <Chip text='leave review' />
+      </span>
+      <span>
+        <img
+          src={`data:image/png;base64, ${product.image}`}
+          alt={product.name}
+        />
       </span>
     </div>
   );
