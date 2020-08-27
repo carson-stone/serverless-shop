30328e4f5bd040b2b81d6764de8ad412dab47054
cstone <cstone@cs.odu.edu>
2020-08-26 23:23:22 -0400

    added filtering function to chips

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,36 +1,62 @@
-import React from 'react';
-import { API } from 'aws-amplify';
+import React, { useState } from 'react';
 import './Home.css';
 import { useAppContext } from '../libs/contextLib';
 import ProductCard from '../components/ProductCard';
 
 export default function Home() {
+  const [selectedButton, setSelectedButton] = useState('all types');
+  const [unselectedButtons, setUnselectedButtons] = useState([
+    'Apple',
+    'Samsung',
+  ]);
+
+  function makeSelected(e) {
+    setSelectedButton(e.target.innerText);
+    setUnselectedButtons([
+      selectedButton,
+      ...unselectedButtons.filter((filter) => filter !== e.target.innerText),
+    ]);
+  }
+
   const { products } = useAppContext();
+
   return (
     <div className='container'>
       <div className='header-row'>
         <h1>Watches</h1>
         <div className='chips'>
-          <button className='chip selected'>
-            <p>all types</p>
-          </button>
-          <button className='chip'>
-            <p>Apple</p>
-          </button>
-          <button className='chip'>
-            <p>Samsung</p>
-          </button>
+          <button className='chip selected'>{selectedButton}</button>
+          {unselectedButtons.map((button) => (
+            <button
+              className='chip'
+              onClick={(e) => makeSelected(e)}
+              key={button}
+            >
+              {button}
+            </button>
+          ))}
         </div>
       </div>
       <div className='Home'>
-        {products.map((product) => (
-          <ProductCard
-            src={`data:image/png;base64, ${product.image}`}
-            name={product.name}
-            price={product.price}
-            key={product.name}
-          />
-        ))}
+        {products.map((product) =>
+          selectedButton === 'all types' ? (
+            <ProductCard
+              src={`data:image/png;base64, ${product.image}`}
+              name={product.name}
+              price={product.price}
+              key={product.name}
+            />
+          ) : (
+            product.name.includes(selectedButton) && (
+              <ProductCard
+                src={`data:image/png;base64, ${product.image}`}
+                name={product.name}
+                price={product.price}
+                key={product.name}
+              />
+            )
+          )
+        )}
       </div>
     </div>
   );
