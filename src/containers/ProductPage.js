3b8a1e2ce8c0a3c242bb12d351891f812da16cf6
cstone <cstone@cs.odu.edu>
2020-08-27 18:30:21 -0400

    included back button on product page

--- src/containers/ProductPage.js
+++ src/containers/ProductPage.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory, Link } from 'react-router-dom';
 import './ProductPage.css';
 import { useAppContext } from '../libs/contextLib';
+import backIcon from '../assets/back-icon.png';
 
 export default function ProductPage() {
   const { name } = useParams();
+  const history = useHistory();
 
   return (
     <div className='ProductPage container'>
-      <h1>{name}</h1>
+      <span>
+        <Link to='/'>
+          <img src={backIcon} alt='back icon' width={35} height={35} />
+        </Link>
+        <h1>{name}</h1>
+      </span>
     </div>
   );
 }
