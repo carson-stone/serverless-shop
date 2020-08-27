f2eeee666d6fb139ce0feb268bfa4ddfa7d33435
cstone <cstone@cs.odu.edu>
2020-08-26 22:52:20 -0400

    added non-functional chips

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -7,15 +7,31 @@ import ProductCard from '../components/ProductCard';
 export default function Home() {
   const { products } = useAppContext();
   return (
-    <div className='Home container'>
-      {products.map((product) => (
-        <ProductCard
-          src={`data:image/png;base64, ${product.image}`}
-          name={product.name}
-          price={product.price}
-          key={product.name}
-        />
-      ))}
+    <div className='container'>
+      <div className='header-row'>
+        <h1>Watches</h1>
+        <div className='chips'>
+          <button className='chip selected'>
+            <p>all types</p>
+          </button>
+          <button className='chip'>
+            <p>Apple</p>
+          </button>
+          <button className='chip'>
+            <p>Samsung</p>
+          </button>
+        </div>
+      </div>
+      <div className='Home'>
+        {products.map((product) => (
+          <ProductCard
+            src={`data:image/png;base64, ${product.image}`}
+            name={product.name}
+            price={product.price}
+            key={product.name}
+          />
+        ))}
+      </div>
     </div>
   );
 }
